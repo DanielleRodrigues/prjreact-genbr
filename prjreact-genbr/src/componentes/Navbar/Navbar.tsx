0e80202f29bfd946f64b3dbe0b815ca2b17c6a24
daniellerodrigues <daniellerodrigues-14@live.com>
2022-11-23 12:30:26 -0300

    add navbar

---
+++ prjreact-genbr/src/componentes/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  './Navbar.css';
+
+
+
+
+
+function Navbar() {
+  return (
+        <div showcase-top={Navbar}>
+        <img src="https://i.ibb.co/r5krrdz/logo.png" alt="" />
+        <button btn-rounded={Navbar}>Sign In</button>
+        </div>
+  )
+}
+
+export  {Navbar}
